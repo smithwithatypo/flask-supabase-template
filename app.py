321f8f259616c0db1d9487bce84abd65203a2309
smithwithatypo <smithwithatypo@gmail.com>
2025-01-28 07:21:56 -0600

    stable release tested

--- app.py
+++ app.py
@@ -1,24 +1,34 @@
 from flask import Flask, request, jsonify
-from supabase import create_client, Client
+from supabase import create_client
 import os
+from dotenv import load_dotenv
 
 app = Flask(__name__)
+
+load_dotenv()
 
 # Supabase configuration
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-@app.route('/add', methods=['POST'])
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    response = "hello world"
+    return response
+
+@app.route('/messages', methods=['GET'])
+def get_data():
+    response = supabase.table('your_table_name').select('*').execute()
+    return jsonify(response.data)
+
+@app.route('/add_message', methods=['POST'])
 def add_data():
     data = request.json
     response = supabase.table('your_table_name').insert(data).execute()
-    return jsonify(response.data), response.status_code
+    return jsonify(response.data)
 
-@app.route('/get', methods=['GET'])
-def get_data():
-    response = supabase.table('your_table_name').select('*').execute()
-    return jsonify(response.data), response.status_code
 
 if __name__ == '__main__':
     app.run(debug=True)
